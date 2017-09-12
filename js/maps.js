21e3580f819c785c2c23678b0266856dfde9c8f1
lorinerosenberg <lennylourosenberg@gmail.com>
2017-09-12 15:36:23 +0300

    initial functions

--- js/maps.js
+++ js/maps.js
@@ -1,10 +1,19 @@
+// global variables for node labels
+
+var parent = {label: 'General Principals...', fullText: "General Principals: Can we ensure that AI is transparent?", group: 'answer'};
+var selected = {label: 'Etzioni (NB- Member of ...', fullText: "Etzioni (NB- Member of IEEE committee, LinkedIn )- yes - an A.I. system must clearly disclose that it is not human. As we have seen in the case of bots — computer programs that can engage in increasingly sophisticated dialogue with real people — society needs assurances that A.I. systems are clearly labeled as such. In 2016, a bot known as Jill Watson, which served as a teaching assistant for an online course at Georgia Tech, fooled students into thinking it was human. A more serious example is the widespread use of pro-Trump political bots on social media in the days leading up to the 2016 elections, according to researchers at Oxford.", group:'supporting'};
+var child1 = {label:'If so then we should also have...', fullText: "If so then we should also have statutory labels whenever there is photoshop or comedic dubbing", group: 'supporting' };
+var child2 = {label: 'That should also include ...', fullText: "That should also include automated transcripts from voice recognition on videos", group: 'supporting'};
+var child3 = {label: 'Disagree. If we take the ..', fullText: "Disagree. If we take the example of photoshop, the regulation varies from country to country. Australia for example does not regulate whereas UK and Israel does. Even if Etzioni is right for bots, it does not mean we need to change the law for photoshop", group: 'disproving'}
+
+
 // create an array with nodes
 var nodes = new vis.DataSet([
-    {id: 'parent', label: 'General Principals...', group: 'answer'},
-    {id: 'selected', label: 'Etzioni (NB- Member of ...', group: 'selected'},
-    {id: 'child1', label: 'If so then we should also have...', group: 'supporting'},
-    {id: 'child2', label: 'That should also include ...', group: 'supporting'},
-    {id: 'child3', label: 'Disagree. If we take the ..', group: 'disproving'}
+    {id: 'parent', label: parent.label, group: parent.group, fullText: parent.fullText},
+    {id: 'selected', label: selected.label, group: 'selected', fullText: selected.fullText},
+    {id: 'child1', label: child1.label , group: child1.group, fullText: child1.fullText },
+    {id: 'child2', label: child2.label, group: child2.group, fullText: child2.fullText },
+    {id: 'child3', label: child3.label, group: child3.group, fullText: child3.fullText }
 ]);
 
 
@@ -71,7 +80,6 @@ var options = {
         navigationButtons: true,
         selectConnectedEdges: true,
         zoomView: true
-
     },
 
     // hierarchy
@@ -126,18 +134,7 @@ var options = {
             springConstant: 0.05,
             nodeDistance: 100,
             damping: 0.09
-        },
-        maxVelocity: 50,
-        minVelocity: 0.1,
-        stabilization: {
-            enabled: true,
-            iterations: 1000,
-            updateInterval: 100,
-            onlyDynamicEdges: false,
-            fit: true
-        },
-        timestep: 0.5,
-        adaptiveTimestep: true
+        }
     }
 };
 
@@ -155,5 +152,33 @@ $( document ).ready(function() {
     // heading text to page question
     var question = "General Principals: Can we ensure that AI is transparent?";
     $(".question").html(question);
+
+    // show full text of selected node
+    // TODO: change from alert box to dialog box
+    function showText(node){
+        var text = nodes.get(node)[0].fullText;
+        alert(text);
+    }
+
+    // change central node to selected node
+    // TODO: need to switch all data around according
+    // TODO: parent node changes to old selected
+    // TODO: child node numbers dependant
+    function selectNode(node){
+        var label = nodes.get(node)[0].label;
+        var group = nodes.get(node)[0].group;
+        nodes.update({id: 'selected', label: label, group: group });
+    }
+
+    network.on('click', function(event){
+        var node = event.nodes;
+        showText(node);
+    });
+    network.on('doubleClick', function(event){
+        var node = event.nodes;
+        selectNode(node);
+    })
+
+
 });
 
