ddfa62dc9031789c58318218b97c0e92bf737318
lorinerosenberg <lennylourosenberg@gmail.com>
2017-09-18 11:43:29 +0300

    backward navigation fix

--- js/graph.js
+++ js/graph.js
@@ -16,6 +16,22 @@ var info = {
         'fullText': null,
         'group': 'answer'
     },
+    11: {
+        'id' : 11,
+        'parent': 1,
+        'children': null,
+        'shortText': 'General Principals: How can we extend the benefits and minimize the risks of AI technology being misused?',
+        'fullText': null,
+        'group': 'answer'
+    },
+    12: {
+        'id' : 12,
+        'parent': 1,
+        'children': null,
+        'shortText': 'Talmud test trial. Silver acquires gold',
+        'fullText': null,
+        'group': 'answer'
+    },
     100: {
         'id' : 100,
         'parent': 10,
@@ -30,7 +46,7 @@ var info = {
         'children': [10000,10001,10002],
         'shortText': 'If so then we should also have statutory labels whenever there is photoshop or comedic dubbing',
         'fullText': null,
-        'group': 'disproving'
+        'group': 'approving'
     },
     1001: {
         'id' : 1001,
@@ -226,233 +242,3 @@ var info = {
     }
 
 };
-
-$(document).ready(function() {
-
-    var container;
-    var nodes;
-    var edges;
-    var data;
-    var network;
-    var selectedNode;
-    var parentNode;
-    var childrenNodes;
-    var nodeData;
-    var edgeData;
-    var parent;
-    var selected;
-    var questionNode = info[10].shortText;
-
-
-    // heading text to page question
-    $(".question").html(questionNode);
-    getData(100);
-
-    // mindmap functionality
-    function getData(selectedID) {
-
-        selectedNode = info[selectedID];
-        parentNode = selectedNode.parent; //returns an id [100]
-        childrenNodes = selectedNode.children; // returns one value or array [1010, 1011]
-        nodeData = [];
-        edgeData = [];
-
-        // node data
-        parent = {
-            id: parentNode,
-            label: info[parentNode].shortText,
-            fullText: info[parentNode].fullText,
-            group: info[parentNode].group
-        };
-        selected = {
-            id: 'selected',
-            label: selectedNode.shortText,
-            fullText: selectedNode.fullText,
-            group: selectedNode.group
-        };
-
-        // formatting and pushing childNode data into an array
-        if (childrenNodes != null) {
-            for (var i in childrenNodes) {
-                var childNode = childrenNodes[i];
-                var childID = selectedID;
-                var edgeID = selectedID;
-
-                window['child' + childID + i] = {
-                    id: childID + i,
-                    label: info[childNode].shortText,
-                    fullText: info[childNode].fullText,
-                    group: info[childNode].group,
-                    childNode: info[childNode].children
-                };
-                nodeData.push(window['child' + childID + i]);
-
-                window['edge' + edgeID + i] = {
-                    id: edgeID + i,
-                    from: 'selected',
-                    to: childID + i
-                };
-                edgeData.push(window['edge' + edgeID + i]);
-            }
-        }
-        else {
-            return null;
-        }
-
-        nodeData.push(parent, selected);
-        edgeData.push({from: parentNode, to: 'selected', smooth: false});
-
-
-        // create dataSet from predefined data above
-        nodes = new vis.DataSet(nodeData);
-        // create an array with edges
-        edges = new vis.DataSet(edgeData);
-        // create a network
-        container = document.getElementById('mynetwork');
-        // provide the data in the vis format
-        data = {
-            nodes: nodes,
-            edges: edges
-        };
-
-
-        // option details
-        var options = {
-
-            autoResize: true,
-            height: '100%',
-
-            // edges options and styles
-            edges: {
-                arrows: {
-                    to: {enabled: true, scaleFactor: 0.7, type: 'arrow'}
-                },
-                arrowStrikethrough: true,
-                color: {
-                    inherit: 'to',
-                    opacity: 1.0
-                },
-                physics: true,
-                scaling: {
-                    min: 1,
-                    max: 15
-                },
-                selectionWidth: 2,
-                selfReferenceSize: 20,
-                smooth: {
-                    enabled: true,
-                    type: "curvedCW",
-                    roundness: 0.5
-                }
-            },
-
-            // groups options and styles (like classes)
-            groups: {
-                supporting: {color: {background: 'white', border: '#2100c4'}},
-                disproving: {color: {background: 'white', border: '#fe2300'}},
-                answer: {color: {background: '#f6f6f6', border: '#9e9e9e'}},
-                question:  {color: {background: '#f6f6f6', border: '#9e9e9e'}}
-            },
-
-            // mouse and touch events
-            interaction: {
-                dragNodes: true,
-                dragView: true,
-                navigationButtons: true,
-                selectConnectedEdges: true,
-                zoomView: true
-            },
-
-            // hierarchy
-            layout: {
-                improvedLayout: true,
-                hierarchical: {
-                    enabled: true,
-                    levelSeparation: 300,
-                    nodeSpacing: 300,
-                    treeSpacing: 200,
-                    blockShifting: true,
-                    parentCentralization: true,
-                    direction: 'LR',        // UD, DU, LR, RL
-                    sortMethod: 'directed'   // hubsize, directed
-                }
-            },
-
-            // nodes options and styles
-            nodes: {
-                font: {
-                    color: '#343434',
-                    size: 18, // px
-                    face: 'arial'
-                },
-                heightConstraint: {
-                    minimum: 70,
-                    valign: 'middle'
-                },
-                mass: 1,
-                physics: true,
-                scaling: {
-                    label: {
-                        enabled: true,
-                        min: 14,
-                        max: 18,
-                        maxVisible: 18,
-                        drawThreshold: 8
-                    }
-                },
-                shape: 'box',
-                shapeProperties: {
-                    borderRadius: 6
-                },
-                chosen: false
-            },
-
-            // movement of nodes and edges
-            physics: {
-                enabled: true,
-                hierarchicalRepulsion: {
-                    centralGravity: 0,
-                    springLength: 500,
-                    springConstant: 0.05,
-                    nodeDistance: 250,
-                    damping: 0.09
-                }
-            }
-        };
-
-
-
-        // initialize network
-        network = new vis.Network(container, data, options);
-        network.setOptions(options);
-
-
-
-        network.on('doubleClick', function (event) {
-            var clickedNode = event.nodes;
-            getData(clickedNode);
-        });
-
-        network.on('click', function(event){
-            var selectedNode = event.nodes;
-            var text = nodes.get(selectedNode)[0].fullText;
-            if(text != null) {
-                $(".full-text").html(text);
-            }
-            else{
-                return null;
-            }
-        });
-
-
-    }
-
-    network.on( 'click', function(properties) {
-        var ids = properties.nodes;
-        var clickedNodes = nodes.get(ids);
-        console.log('clicked nodes:', clickedNodes);
-    });
-
-
-
-});
